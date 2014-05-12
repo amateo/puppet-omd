4c9085dcf56cdccf241c0375e1a6c2dce62863e8
Angel L. Mateo <amateo@um.es>
2014-05-12 12:57:05 +0200

    When installing in debian, add repository before package installation

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,12 @@
 # == Class omd::install
 #
 class omd::install {
+  case $::osfamily {
+    Debian: {
+      class {'omd::install::debian':} ->
+      Class['omd::install']
+    }
+  }
 
   $managed_package_ensure = $omd::ensure ? {
     present => $omd::version,
