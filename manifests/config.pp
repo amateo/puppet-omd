159cbaceda9f318cbf1f698d341f4b8946366e9f
Angel L. Mateo <amateo@um.es>
2014-05-12 14:33:26 +0200

    Manage configuration of sites

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,63 +3,6 @@
 # This class is called from omd
 #
 class omd::config {
-
-  $managed_file_content = $omd::file_content ? {
-    undef   => $omd::file_template ? {
-      undef   => undef,
-      default => template($omd::file_template),
-    },
-    default => $omd::file_content,
-  }
-
-  $managed_file_source = $omd::file_source ? {
-    ''      => undef,
-    default => $omd::file_source,
-  }
-
-  $managed_dir_ensure = $omd::ensure ? {
-    'absent'  => 'absent',
-    'present' => 'directory',
-  }
-
-  $managed_file_replace = $omd::audit_only ? {
-    true  => false,
-    false => true,
-  }
-
-  $managed_audit = $omd::audit_only ? {
-    true  => 'all',
-    false => undef,
-  }
-
-  #
-  # Manage single file configuration
-  if $omd::file {
-    file { 'omd.conf':
-      ensure  => $omd::ensure,
-      path    => $omd::file,
-      mode    => $omd::file_mode,
-      owner   => $omd::file_owner,
-      group   => $omd::file_group,
-      source  => $omd::config::managed_file_source,
-      content => $omd::config::managed_file_content,
-      replace => $omd::config::managed_file_replace,
-      audit   => $omd::config::managed_audit,
-    }
-  }
-
-  #
-  # Manage whole config dir
-  if $omd::dir_source {
-    file { 'omd.dir':
-      ensure  => $omd::config::managed_dir_ensure,
-      path    => $omd::dir,
-      source  => $omd::dir_source,
-      recurse => true,
-      purge   => $omd::dir_purge,
-      force   => $omd::dir_purge,
-      replace => $omd::config::managed_file_replace,
-      audit   => $omd::config::managed_audit,
-    }
-  }
+  Mount <| tag == 'omd::site' |>
+  Exec <| tag == 'omd::site' |>
 }
---
+++ manifests/site.pp
@@ -0,0 +1,59 @@
+define omd::site (
+  $ensure = 'present',
+) {
+  case $ensure {
+    'present': {
+      @exec { "create_site_${name}":
+        command => "omd create ${name}",
+        path    => '/usr/bin',
+        unless  => "omd sites -b | /bin/grep -q '^${name}$'",
+        creates => "/opt/omd/sites/${name}",
+        tag     => 'omd::site',
+      }
+    }
+    'absent': {
+      #
+      # Esto es un poco puñeta, pero el "omd rm <site>" es interactivo",
+      # así que toca deshabilitarlo, desmontar el tmpfs y borrar a mano
+      #
+      @mount {"/omd/sites/${name}/tmp":
+        ensure => 'absent',
+        tag    => 'omd::site',
+      }
+
+      @exec { "remove_site_${name}":
+        command  => "omd disable ${name} && /bin/rm -rf /opt/omd/sites/${name} && /usr/sbin/userdel ${name} && /usr/sbin/groupdel ${name}",
+        path     => '/usr/bin',
+        onlyif   => "omd sites -b | /bin/grep -q '^${name}$'",
+        tag      => 'omd::site',
+        require  => Mount['/omd/sites/kk1/tmp'],
+      }
+    }
+    default: {
+      fail("ensure parameter ($ensure) must be \'present\' or \'absent\'")
+    }
+  }
+
+  $manage_service_enabled = $omd::ensure ? {
+    'absent' => undef,
+    default  => $mod::service_enable,
+  }
+
+  $manage_service_ensure = $omd::ensure ? {
+    'absent' => 'stopped',
+    default  => $omd::service_ensure,
+  }
+
+
+  @service { "site_service_${name}":
+    ensure     => $manage_service_ensure,
+    enable     => $manage_service_enable,
+    hasrestart => true,
+    hasstatus  => true,
+    restart    => "/usr/bin/omd restart ${name}",
+    start      => "/usr/bin/omd start ${name}",
+    status     => "/usr/bin/omd status ${name}",
+    stop       => "/usr/bin/omd stop ${name}",
+    tag        => 'omd::site',
+  }
+}
