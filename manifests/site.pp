eef4217174bde567927d770621c584cd176fc30d
Angel L. Mateo <amateo@um.es>
2014-05-14 13:57:17 +0200

    Cuando se activa el site en modo 'shared' (no se crea una propia instancia de apache, sino que se activa a nivel global del apache), genero los ficheros de configuración de apache necesarios.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,26 @@
 define omd::site (
+  $site   = '',
   $ensure = 'present',
+  $mode   = 'own',
 ) {
+  validate_re($mode, '^(own|shared)$',
+    'mode parameter must be one of \'own\' or \'shared\'')
+
+  $sitename = $site ? {
+    ''      => $name,
+    default => $site,
+  }
+
   #
   # Create/Remove site
-  # 
+  #
   case $ensure {
     'present': {
       @exec { "create_site_${name}":
-        command => "omd create ${name}",
+        command => "omd create ${sitename}",
         path    => '/usr/bin',
-        unless  => "omd sites -b | /bin/grep -q '^${name}$'",
-        creates => "/opt/omd/sites/${name}",
+        unless  => "omd sites -b | /bin/grep -q '^${sitename}$'",
+        creates => "/opt/omd/sites/${sitename}",
         tag     => 'omd::site::config',
       }
 
@@ -26,38 +36,72 @@ define omd::site (
 
       @service { "site_service_${name}":
         ensure     => $manage_service_ensure,
-        enable     => $manage_service_enable,
+        enable     => $manage_service_enabled,
         hasrestart => true,
         hasstatus  => true,
-        restart    => "/usr/bin/omd restart ${name}",
-        start      => "/usr/bin/omd start ${name}",
-        status     => "/usr/bin/omd status ${name}",
-        stop       => "/usr/bin/omd stop ${name}",
+        restart    => "/usr/bin/omd restart ${sitename}",
+        start      => "/usr/bin/omd start ${sitename}",
+        status     => "/usr/bin/omd status ${sitename}",
+        stop       => "/usr/bin/omd stop ${sitename}",
         provider   => 'base',
         tag        => 'omd::site::service',
         require    => Exec["create_site_${name}"],
       }
+
+      if $mode == 'shared' {
+        apache::dotconf{"02_fcgid_${name}":
+          path     => "/omd/sites/${sitename}/etc/apache/conf.d/",
+          owner    => $sitename,
+          group    => $sitename,
+          mode     => '0644',
+          template => 'omd/fcgid_site.conf.erb',
+          require  => [ Exec["create_site_${name}"],
+                        File["/omd/sites/${sitename}/etc/apache/conf.d/02_fcgid.conf"],
+          ]
+        }
+
+        apache::dotconf {"mode_${name}":
+          path     => "/opt/omd/sites/${sitename}/etc/apache",
+          owner    => $sitename,
+          group    => $sitename,
+          mode     => '0644',
+          template => 'omd/mode_shared.conf.erb',
+          require  => [ Exec["create_site_${name}"],
+                        File["/omd/sites/${sitename}/etc/apache/mode.conf"],
+          ]
+        }
+
+        file {"/omd/sites/${sitename}/etc/apache/conf.d/02_fcgid.conf":
+          ensure => 'absent',
+        }
+
+        file {"/omd/sites/${sitename}/etc/apache/mode.conf":
+          ensure => 'link',
+          target => "mode_${name}.conf",
+        }
+      }
+
     }
     'absent': {
       #
       # Esto es un poco puñeta, pero el "omd rm <site>" es interactivo",
       # así que toca deshabilitarlo, desmontar el tmpfs y borrar a mano
       #
-      @mount {"/omd/sites/${name}/tmp":
+      @mount {"/omd/sites/${sitename}/tmp":
         ensure => 'absent',
         tag    => 'omd::site::config',
       }
 
       @exec { "remove_site_${name}":
-        command  => "omd disable ${name} && /bin/rm -rf /opt/omd/sites/${name} && /usr/sbin/userdel ${name} && /usr/sbin/groupdel ${name}",
+        command  => "omd disable ${sitename} && /bin/rm -rf /opt/omd/sites/${sitename} && /usr/sbin/userdel ${sitename} && /usr/sbin/groupdel ${sitename}",
         path     => '/usr/bin',
-        onlyif   => "omd sites -b | /bin/grep -q '^${name}$'",
+        onlyif   => "omd sites -b | /bin/grep -q '^${sitename}$'",
         tag      => 'omd::site::config',
         require  => Mount['/omd/sites/kk1/tmp'],
       }
     }
     default: {
-      fail("ensure parameter ($ensure) must be \'present\' or \'absent\'")
+      fail("ensure parameter (${ensure}) must be \'present\' or \'absent\'")
     }
   }
 }
