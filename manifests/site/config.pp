aac8e8cdd720f36294be677dedb4371f06bda705
Angel L. Mateo <amateo@um.es>
2014-05-15 08:29:32 +0200

    Para poder manejar la configuración de varios sites, la opción que queremos configurar no puede ser el title del resource, así que obligo a indicar el parámetro option. También obligo a indicar el parámetro value.

--- manifests/site/config.pp
+++ manifests/site/config.pp
@@ -1,18 +1,13 @@
 define omd::site::config (
   $site,
-  $option = '',
-  $value  = '',
+  $option,
+  $value,
 ) {
-  $manage_option = $option ? {
-    ''      => $name,
-    default => $option,
-  }
-
   $site_file = "/opt/omd/sites/${site}/etc/omd/site.conf"
 
   @exec {"config_${name}_${site}":
-    command => "/bin/sed -i \"s/^${manage_option}=.\\+$/${manage_option}='${value}'/\" ${site_file}",
-    unless  => "/bin/egrep -q \"^${manage_option}='${value}'$\" ${site_file}",
+    command => "/bin/sed -i \"s/^${option}=.\\+$/${option}='${value}'/\" ${site_file}",
+    unless  => "/bin/egrep -q \"^${option}='${value}'$\" ${site_file}",
     require => Exec["create_site_${site}"],
     tag     => 'omd::site::config',
   }
