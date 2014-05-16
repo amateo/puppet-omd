496613d15463d75b7cb22ab148ba7acfce495db1
Angel L. Mateo <amateo@um.es>
2014-05-16 14:45:35 +0200

    Instancio también el resto de objetos nagios.

--- manifests/site/nagios.pp
+++ manifests/site/nagios.pp
@@ -32,12 +32,61 @@ define omd::site::nagios (
   # Instanciamos todos los resources nagios_XXX definidos para este servidor
   # y este site
   #
-#  Nagios_host <<| tag == "nagios-${sitename}" |>> {
-#    notify => Exec["reload nagios ${name}"],
-#  }
-  Nagios_contact <<| tag == "nagios-${sitename}" |>> {
+  Nagios_command <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/commands.cfg",
+  }
+  Nagios_contact <<| tag == "omd-nagios-${sitename}" |>> {
     notify => Exec["reload nagios ${name}"],
     target => "${nagiosdir}/conf.d/contacts.cfg",
+  }
+  Nagios_contactgroup <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/contactgroups.cfg",
+  }
+  Nagios_host <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/hosts.cfg",
+  }
+  Nagios_hostdependency <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/hostdependency.cfg",
+  }
+  Nagios_hostescalation <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/hostescalation.cfg",
+  }
+  Nagios_hostextinfo <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/hostextinfo.cfg",
+  }
+  Nagios_hostgroup <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/hostgroups.cfg",
+  }
+  Nagios_service <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/services.cfg",
+  }
+  Nagios_servicedependency <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/servicedependency.cfg",
+  }
+  Nagios_serviceescalation <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/serviceescalation.cfg",
+  }
+  Nagios_serviceextinfo <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/serviceextinfo.cfg",
+  }
+  Nagios_servicegroup <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/servicegroups.cfg",
+  }
+  Nagios_timeperiod <<| tag == "omd-nagios-${sitename}" |>> {
+    notify => Exec["reload nagios ${name}"],
+    target => "${nagiosdir}/conf.d/timeperiods.cfg",
   }
 
 }
