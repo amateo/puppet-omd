d7478f6ab861018fb8915dba4b29710b4fb3bad7
Angel L. Mateo <amateo@um.es>
2014-06-04 14:01:00 +0200

    Para gestionar los refrescos, voy a volver a definirlo todo como virtual resources, así es más sencillo de configurar dependencias.

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,5 +5,5 @@
 class omd::config {
   #Mount <| tag == 'omd::site::config' |>
   #Exec <| tag == 'omd::site::config' |>
-  #File <| tag == 'omd::site::config' |>
+  File <| tag == 'omd::site::config' |>
 }
