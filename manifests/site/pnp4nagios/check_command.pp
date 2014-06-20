f1c2be1c24c0a59f9c0b991790555060b5e6c6d0
Angel L. Mateo <amateo@um.es>
2014-06-20 11:46:48 +0200

    Define (y su template) para crear check_commands de pnp4nagios (mapeo de comando nagios a plantilla pnp4nagios)

---
+++ manifests/site/pnp4nagios/check_command.pp
@@ -0,0 +1,31 @@
+define omd::site::pnp4nagios::check_command (
+  $site,
+  $ensure = 'present',
+  $command = undef,
+  $custom_template = 1,
+  $options = {},
+  $path = undef,
+) {
+  validate_hash($options)
+
+  $_command = $command ? {
+    undef   => $name,
+    default => $command,
+  }
+
+  $sitedir = "/omd/sites/${site}"
+
+  $_path = $path ? {
+    undef   => "${sitedir}/etc/pnp4nagios/check_commands/${_command}.cfg",
+    default => $path,
+  }
+
+  file { "pnp4nagios_checkcommand_${name}":
+    ensure  => $ensure,
+    path    => $_path,
+    owner   => $site,
+    group   => $site,
+    mode    => '0644',
+    content => template('omd/site/pnp4nagios/check_command.cfg.erb'),
+  }
+}
