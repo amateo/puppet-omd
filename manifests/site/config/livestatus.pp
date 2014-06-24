d5ee049c0cb1b66c3176a988ed8dbdc55a5d80a2
Angel L. Mateo <amateo@um.es>
2014-06-24 13:22:20 +0200

    Quito esta configuración que no estaba utilizando y no es necesaria

--- manifests/site/config/livestatus.pp
+++ manifests/site/config/livestatus.pp
@@ -23,11 +23,4 @@ define omd::site::config::livestatus (
     path   => "${sitedir}/etc/xinetd.d/mk-livestatus",
     target => '../mk-livestatus/xinetd.conf'
   }
-
-  #augeas { "${_site}_livestatus_only_from":
-    #context => "/files/${sitedir}/etc/mk-livestatus/xinetd.conf/service/",
-    #changes => "set only_from 127.0.0.1",
-    #lens    => 'xinetd.lns',
-    #incl    => "${sitedir}/etc/mk-livestatus/xinetd.conf",
-  #}
 }
