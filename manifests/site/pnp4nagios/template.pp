e39ba24ae85444ca385abaef8a220d63777d56a4
Angel L. Mateo <amateo@um.es>
2014-07-03 10:31:26 +0200

    Quito mensajes de debug

--- manifests/site/pnp4nagios/template.pp
+++ manifests/site/pnp4nagios/template.pp
@@ -44,8 +44,6 @@ define omd::site::pnp4nagios::template (
     default => $content,
   }
 
-  notify {"$name -> $path":}
-
   file { "pnp4nagios_template_${name}":
     ensure  => $ensure,
     path    => $_path,
