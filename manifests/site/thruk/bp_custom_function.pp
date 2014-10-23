f7c3ad38626d8004a49454a753bb0ad8469dab4a
Angel L. Mateo <amateo@um.es>
2014-10-23 12:12:18 +0200

    Este define es para poder añadir custom_functions de BP al thruk

---
+++ manifests/site/thruk/bp_custom_function.pp
@@ -0,0 +1,51 @@
+define omd::site::thruk::bp_custom_function (
+  $site,
+  $ensure   = 'present',
+  $source   = undef,
+  $content  = undef,
+  $template = undef,
+  $owner    = undef,
+  $group    = undef,
+  $mode     = '0644',
+  $path     = undef,
+) {
+  if !$source and !$content and !$template {
+    fail('You must provide one of source, content or template parameters')
+  }
+
+  $_owner = $owner ? {
+    undef   => $site,
+    default => $owner,
+  }
+
+  $_group = $group ? {
+    undef   => $site,
+    default => $group,
+  }
+
+  $sitedir = "/omd/sites/${site}"
+
+  $_path = $path ? {
+    undef   => "${sitedir}/etc/thruk/bp/${name}",
+    default => $path,
+  }
+
+  $_content = $content ? {
+    undef   => $template ? {
+      undef   => undef,
+      default => template($template),
+    },
+    default => $content,
+  }
+
+  file { "thruk::custom_function::${name}":
+    ensure  => $ensure,
+    path    => $_path,
+    owner   => $_owner,
+    group   => $_group,
+    mode    => $mode,
+    source  => $source,
+    content => $_content,
+  }
+}
+
