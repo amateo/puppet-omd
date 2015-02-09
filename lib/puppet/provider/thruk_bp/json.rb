dfba9a76f89221ebe98c358e2a580a106bf72462
Angel L. Mateo <amateo@um.es>
2015-02-09 12:54:16 +0100

    TLM-1073: Añado la generación/borrado de los objetos nagios y que se asegure de que está el cron que se encarga de chequear los BPs

--- lib/puppet/provider/thruk_bp/json.rb
+++ lib/puppet/provider/thruk_bp/json.rb
@@ -1,5 +1,4 @@
 require 'json'
-require 'yaml'
 
 begin
   require 'puppet_x/omd/thruk'
@@ -57,8 +56,6 @@ Puppet::Type.type(:thruk_bp).provide(:ruby) do
 
   def flush
     #save_to_disk
-    #save_nagios_objects
-    #ensure_cron
     if @property_flush[:ensure] == :absent
       File.delete(@property_hash[:target]) if File.file?(@property_hash[:target])
       File.delete(@property_hash[:bp_target]) if File.file?(@property_hash[:bp_target])
@@ -72,6 +69,8 @@ Puppet::Type.type(:thruk_bp).provide(:ruby) do
 
       to_json(@property_hash)
     end
+    save_nagios_objects
+    ensure_cron
   end
 
   #
@@ -124,6 +123,118 @@ Puppet::Type.type(:thruk_bp).provide(:ruby) do
     filename = @property_hash[:target]
     File.open(filename, 'w') do |f|
       f.puts JSON.pretty_generate(@property_hash)
+    end
+  end
+
+  def save_nagios_objects
+    # Abrimos el fichero con augeas
+    path = '/omd/sites/' + resource[:site] + '/etc/nagios/conf.d/thruk_bp_generated.cfg'
+    aug = Augeas::open(nil, nil, Augeas::NO_MODL_AUTOLOAD)
+    aug.transform(:lens => "nagiosobjects.lns", :incl => path)
+    aug.load
+
+    if match = @property_hash[:bp_target].match(/^.+\/([^\/]+)\.tbp/)
+      bp_id = match[1]
+    end
+    host_entry = nil
+    service_entry = nil
+    last_host = 0
+    last_service = 0
+    aug.match('/files/' + path + '/*').each do |entry|
+      if entry.match(/^.+\/host(\[\d+\])?$/)
+        host_number = (match = entry.match(/^.+\/host\[(\d+)\]$/)) ? match[1].to_i : 1
+        last_host = host_number if host_number > last_host
+        entry_id = aug.get(entry + '/_THRUK_BP_ID')
+        if entry_id == bp_id
+          host_entry = entry
+        end
+      elsif entry.match(/^.+\/service(\[\d+\])?$/)
+        service_number = (match = entry.match(/^.+\/service\[(\d+)\]$/)) ? match[1].to_i : 1
+        last_service = service_number if service_number > last_service
+        entry_id = aug.get(entry + '/_THRUK_BP_ID')
+        if entry_id == bp_id
+          service_entry = entry
+        end
+      end
+    end
+    if @property_flush[:ensure] == :absent
+      aug.rm(host_entry) if host_entry
+      aug.rm(service_entry) if service_entry
+    else
+      aug_host_path = host_entry ?
+        host_entry : '/files' + path + '/host[' + (last_host + 1).to_s + ']'
+      aug_service_path = service_entry ?
+        service_entry : '/files' + path + '/service[' + (last_service + 1).to_s + ']'
+      host_template = resource[:host_template] ? resource[:host_template] : 'thruk-bp-template'
+      service_template = resource[:service_template] ? resource[:service_template] : 'thruk-bp-node-template'
+      aug.set(aug_host_path + '/use', host_template)
+      aug.set(aug_host_path + '/host_name', resource[:host_name])
+      aug.set(aug_host_path + '/alias', 'Business Process: ' + resource[:host_name])
+      aug.set(aug_host_path + '/_THRUK_BP_ID', bp_id)
+      aug.set(aug_host_path + '/_THRUK_NODE_ID', 'node1')
+      aug.set(aug_service_path + '/use', service_template)
+      aug.set(aug_service_path + '/host_name', resource[:host_name])
+      aug.set(aug_service_path + '/service_description', resource[:name])
+      aug.set(aug_service_path + '/display_name', resource[:name])
+      aug.set(aug_service_path + '/_THRUK_BP_ID', bp_id)
+      aug.set(aug_service_path + '/_THRUK_NODE_ID', 'node1')
+    end
+
+    aug.save
+    aug.close
+
+    # Reload nagios
+    system('/usr/bin/omd reload  ' + resource[:site] + ' nagios')
+  end
+
+  def ensure_cron
+    path = '/omd/sites/' + resource[:site] + '/etc/cron.d/thruk.auto'
+    version = get_site_version
+    changed = false
+    if File.zero?(path)
+      file = File.open(path, 'w')
+      file.puts('# THIS PART IS WRITTEN BY THRUK, CHANGES WILL BE OVERWRITTEN')
+      file.puts('##############################################################')
+      file.puts('# business process')
+      file.puts("* * * * * cd /opt/omd/versions/" + version + "/share/thruk && /bin/bash -l -c '/omd/sites/" + resource[:site] + "/bin/thruk -a bpd' >/dev/null 2>>/omd/sites/" + resource[:site] + "/var/thruk/cron.log")
+      file.puts('##############################################################')
+      file.puts('# END OF THRUK')
+      file.close
+      changed = true
+    else
+      first_mark = false
+      file = File.open(path, 'r+')
+      file.each do |line|
+        if line.match(/^#+$/)
+          if !first_mark
+            first_mark = true
+          else
+            # Hemos llegado a la marca de fin, tenemos
+            # que crear la entrada
+            length = 0 - line.length
+            file.seek length, IO::SEEK_CUR
+            file.puts('# business process')
+            file.puts("* * * * * cd /opt/omd/versions/1.10/share/thruk && /bin/bash -l -c '/omd/sites/" + resource[:site] + "/bin/thruk -a bpd' >/dev/null 2>>/omd/sites/" + resource[:site] + "/var/thruk/cron.log")
+            file.puts('##############################################################')
+            file.puts('# END OF THRUK')
+            changed = true
+          end
+        elsif line.match(/^# business process$/)
+          break
+        end
+      end
+      file.close
+    end
+    system('/usr/bin/omd reload ' + resource[:site] + ' crontab') if changed == true
+  end
+
+  def get_site_version
+    command = '/usr/bin/omd version ' + resource[:site]
+    output = `#{command}`
+    if match = output.match(/^OMD - Open Monitoring Distribution Version (\d+\.\d+)$/)
+      return match[1]
+    else
+      return 'default'
     end
   end
 
@@ -219,118 +330,6 @@ Puppet::Type.type(:thruk_bp).provide(:ruby) do
       #file = File.open(get_filename, 'w')
       #file.write(JSON.pretty_generate(to_hash))
       #file.close
-    #end
-  #end
-
-  #def save_nagios_objects
-    ## Abrimos el fichero con augeas
-    #path = '/omd/sites/' + resource[:site] + '/etc/nagios/conf.d/thruk_bp_generated.cfg'
-    #aug = Augeas::open(nil, nil, Augeas::NO_MODL_AUTOLOAD)
-    #aug.transform(:lens => "nagiosobjects.lns", :incl => path)
-    #aug.load
-
-    #if match = get_filename.match(/^.+\/([^\/]+)\.tbp/)
-      #bp_id = match[1]
-    #end
-    #host_entry = nil
-    #service_entry = nil
-    #last_host = 0
-    #last_service = 0
-    #aug.match('/files/' + path + '/*').each do |entry|
-      #if entry.match(/^.+\/host(\[\d+\])?$/)
-        #host_number = (match = entry.match(/^.+\/host\[(\d+)\]$/)) ? match[1].to_i : 1
-        #last_host = host_number if host_number > last_host
-        #entry_id = aug.get(entry + '/_THRUK_BP_ID')
-        #if entry_id == bp_id
-          #host_entry = entry
-        #end
-      #elsif entry.match(/^.+\/service(\[\d+\])?$/)
-        #service_number = (match = entry.match(/^.+\/service\[(\d+)\]$/)) ? match[1].to_i : 1
-        #last_service = service_number if service_number > last_service
-        #entry_id = aug.get(entry + '/_THRUK_BP_ID')
-        #if entry_id == bp_id
-          #service_entry = entry
-        #end
-      #end
-    #end
-    #if @property_flush[:ensure] == :absent
-      #aug.rm(host_entry) if host_entry
-      #aug.rm(service_entry) if service_entry
-    #else
-      #aug_host_path = host_entry ?
-        #host_entry : '/files' + path + '/host[' + (last_host + 1).to_s + ']'
-      #aug_service_path = service_entry ?
-        #service_entry : '/files' + path + '/service[' + (last_service + 1).to_s + ']'
-      #host_template = resource[:host_template] ? resource[:host_template] : 'thruk-bp-template'
-      #service_template = resource[:service_template] ? resource[:service_template] : 'thruk-bp-node-template'
-      #aug.set(aug_host_path + '/use', host_template)
-      #aug.set(aug_host_path + '/host_name', resource[:host_name])
-      #aug.set(aug_host_path + '/alias', 'Business Process: ' + resource[:host_name])
-      #aug.set(aug_host_path + '/_THRUK_BP_ID', bp_id)
-      #aug.set(aug_host_path + '/_THRUK_NODE_ID', 'node1')
-      #aug.set(aug_service_path + '/use', service_template)
-      #aug.set(aug_service_path + '/host_name', resource[:host_name])
-      #aug.set(aug_service_path + '/service_description', resource[:name])
-      #aug.set(aug_service_path + '/display_name', resource[:name])
-      #aug.set(aug_service_path + '/_THRUK_BP_ID', bp_id)
-      #aug.set(aug_service_path + '/_THRUK_NODE_ID', 'node1')
-    #end
-
-    #aug.save
-    #aug.close
-
-    ## Reload nagios
-    #system('/usr/bin/omd reload  ' + resource[:site] + ' nagios')
-  #end
-
-  #def ensure_cron
-    #path = '/omd/sites/' + resource[:site] + '/etc/cron.d/thruk.auto'
-    #version = get_site_version
-    #changed = false
-    #if File.zero?(path)
-      #file = File.open(path, 'w')
-      #file.puts('# THIS PART IS WRITTEN BY THRUK, CHANGES WILL BE OVERWRITTEN')
-      #file.puts('##############################################################')
-      #file.puts('# business process')
-      #file.puts("* * * * * cd /opt/omd/versions/" + version + "/share/thruk && /bin/bash -l -c '/omd/sites/" + resource[:site] + "/bin/thruk -a bpd' >/dev/null 2>>/omd/sites/" + resource[:site] + "/var/thruk/cron.log")
-      #file.puts('##############################################################')
-      #file.puts('# END OF THRUK')
-      #file.close
-      #changed = true
-    #else
-      #first_mark = false
-      #file = File.open(path, 'r+')
-      #file.each do |line|
-        #if line.match(/^#+$/)
-          #if !first_mark
-            #first_mark = true
-          #else
-            ## Hemos llegado a la marca de fin, tenemos
-            ## que crear la entrada
-            #length = 0 - line.length
-            #file.seek length, IO::SEEK_CUR
-            #file.puts('# business process')
-            #file.puts("* * * * * cd /opt/omd/versions/1.10/share/thruk && /bin/bash -l -c '/omd/sites/" + resource[:site] + "/bin/thruk -a bpd' >/dev/null 2>>/omd/sites/" + resource[:site] + "/var/thruk/cron.log")
-            #file.puts('##############################################################')
-            #file.puts('# END OF THRUK')
-            #changed = true
-          #end
-        #elsif line.match(/^# business process$/)
-          #break
-        #end
-      #end
-      #file.close
-    #end
-    #system('/usr/bin/omd reload ' + resource[:site] + ' crontab') if changed == true
-  #end
-
-  #def get_site_version
-    #command = '/usr/bin/omd version ' + resource[:site]
-    #output = `#{command}`
-    #if match = output.match(/^OMD - Open Monitoring Distribution Version (\d+\.\d+)$/)
-      #return match[1]
-    #else
-      #return 'default'
     #end
   #end
 end
